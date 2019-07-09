2029c47f44b54529018509e6b4dfc6abf6e18cab
JOYSHELISHIYA <48687528+JOYSHELISHIYA@users.noreply.github.com>
2019-07-09 22:32:14 +0530

    Update Vowel.py

--- Vowel.py
+++ Vowel.py
@@ -1,5 +1,5 @@
 a = input();
-if((a>'a'and  a<'z')or(a>'A' and  a<'B')):
+if((a>='a'and  a<='z')or(a>='A' and  a<='B')):
     if (a =='A'or a=='a'or  a=='B' or a=='b'or a=='e'or a=='E'or a=='i'or a=='I'or a=='O'or a=='o'):
         print("Vowel");
     else:
