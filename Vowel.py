5964309d404fd39ce0797a4d3eec3ec9fc81608f
JOYSHELISHIYA <48687528+JOYSHELISHIYA@users.noreply.github.com>
2019-07-09 22:16:40 +0530

    Update Vowel.py

--- Vowel.py
+++ Vowel.py
@@ -1,8 +1,11 @@
 a = input();
-if (a =='A'or a=='a'or  a=='B' or a=='b'or a=='e'or a=='E'or a=='i'or a=='I'or a=='O'or a=='o'):
-    print("Vowel");
+if((a>'a'&& a<'z')||(a>'A'&& a<'B')):
+    if (a =='A'or a=='a'or  a=='B' or a=='b'or a=='e'or a=='E'or a=='i'or a=='I'or a=='O'or a=='o'):
+        print("Vowel");
+    else:
+        print("Consonant");
 else:
-  print("Consonent");
+    print("Invalid");
     
       
 
